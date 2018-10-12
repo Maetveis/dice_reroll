246dce3b684cf1fb43ec30b1455f6c7d597f8789
Gergely <maetveis@gmail.com>
2018-10-12 13:59:31 +0200

    Added comments

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <sstream>
 #include <cmath>
 
+
+//Helper functions for C-style arrays
+
 void clear(double* b, unsigned size)
 {
 	for(unsigned i = 0; i < size; ++i)
@@ -9,18 +12,6 @@ void clear(double* b, unsigned size)
 		b[i] = 0;
 	}
 }
-
-double sum(double* b, unsigned size)
-{
-	double sum = 0;
-	for(unsigned i = 0; i < size; ++i)
-	{
-		sum += b[i];
-	}
-
-	return sum;
-}
-
 void copy(double* a, double* b, unsigned size)
 {
 	for(unsigned i = 0; i < size; ++i)
@@ -34,13 +25,17 @@ double rerollProbAll(unsigned faces, unsigned count, unsigned rerolls)
 	double a[count + 1];
 	double b[count + 1];
 
+	//Initialize the probabilities
 	for(unsigned hit = 0; hit <= count; ++hit)
 	{
 		a[count - hit] = 1.0 / pow(faces, hit) * pow((faces - 1.0) / faces, count - hit);
 	}
 
+	//Use the previous level to calculate the next
 	for(unsigned reroll = 1; reroll < rerolls; ++reroll)
 	{
+		//The next level is a Linear combination of the previous
+		//So this double loop is actually a product of a triangle matrix and the previous layer as a vector
 		clear(b, count + 1);
 		b[0] = a[0];
 		for(unsigned i = 1; i <= count; ++i)
@@ -60,6 +55,8 @@ int main(int argv, char** args)
 {
 	unsigned a, b, c;
 
+	//Takes the paramaters from the command line
+	// Usage: ./dice_reroll   faces   number_of_dice   rerolls
 	std::stringstream ss;
 	ss << args[1] << " " << args[2] << " " << args[3];
 	ss >> a;
